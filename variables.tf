b8339b8eec498b3370ace27e43dce7ae3a5ab870
jithesh79 <jitheshks11@gmail.com>
2025-10-03 10:16:01 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" { default = "us-east-1" }
+variable "project_name" { default = "prod-app" }
+variable "vpc_cidr" { default = "10.0.0.0/16" }
+
+variable "public_subnet_a_cidr" { default = "10.0.1.0/24" }
+variable "public_subnet_b_cidr" { default = "10.0.2.0/24" }
+variable "private_subnet_a_cidr" { default = "10.0.3.0/24" }
+variable "private_subnet_b_cidr" { default = "10.0.4.0/24" }
+
+variable "instance_type" { default = "t3.medium" }
+variable "key_name" { description = "Your EC2 Key Pair name" }
